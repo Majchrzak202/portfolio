f7d76c2e5a67f4bcbbaca104d771c6cd181f04b9
MarcinMaster <m.luba505@gmail.com>
2022-06-27 08:44:00 +0200

    Added a Waveing Hand into the Home Comp

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -7,9 +8,26 @@ const Home = () => {
       <div className="home">
         <div>
           <h3>
-            Hi there ^^ <br />
-            I'm Marcin Łuba
+            Hi there {" "}
+            <span>
+              <motion.div className="hand"
+                animate={{ rotate: 15 }}
+                transition={{
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 0.1,
+                  from: 0,
+                  duration: 12,
+                  ease: "easeInOut",
+                  type: "tween",
+                }}
+              >
+                👋
+              </motion.div>
+            </span>
           </h3>
+
+          <h3>I'm Marcin Łuba</h3>
           <h3>Junior React Developer</h3>
         </div>
         <img
@@ -25,8 +43,8 @@ const Home = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique. Duis cursus,
               mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
-              libedddro vitae erat. Aenean faucibus nibh et justo cursus id rutrum
-              lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
+              libedddro vitae erat. Aenean faucibus nibh et justo cursus id
+              rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
             </p>
           </div>
           <div>
@@ -35,31 +53,30 @@ const Home = () => {
               alt="6"
             />
           </div>
-          
         </div>
         <div>
-            <h5>Feel free to contact me</h5>
-            <p>COnect with me </p>
-            <img
-              src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
-              alt="2"
-            />
-            <img
-              src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
-              alt="3"
-            />
-            <img
-              src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
-              alt="4"
-            />
-            <img
-              src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
-              alt="5"
-            />
-          </div>
-          <div>
-            <p>Spotify plugin</p>
-          </div>
+          <h5>Feel free to contact me</h5>
+          <p>COnect with me </p>
+          <img
+            src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
+            alt="2"
+          />
+          <img
+            src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
+            alt="3"
+          />
+          <img
+            src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
+            alt="4"
+          />
+          <img
+            src="https://www.iconsdb.com/icons/preview/icon-sets/sketchy-blue/twitter-xl.png"
+            alt="5"
+          />
+        </div>
+        <div>
+          <p>Spotify plugin</p>
+        </div>
       </div>
     </>
   );
