91d61f8050ca78e574c5c7b594bce0ae0f6bd277
MarcinMaster <m.luba505@gmail.com>
2022-07-17 06:41:54 +0200

    Litle adjustments

--- src/components/consts/projectList.js
+++ src/components/consts/projectList.js
@@ -1,34 +1,36 @@
 export const PROJECTS = [
   {
     id: 1,
-    title: "Best app i ever have!",
-    descritpion: "This is the coolest app ever made",
-    img: "https://ui-lib.com/blog/wp-content/uploads/2022/04/john-minimal-one-page-1.png",
+    title: "Simply Weather App",
+    descritpion: "Weather forecast React App ",
+    longDescritption:
+      "Simple React Weather App that uses React Routing, Custom Hooks and connects to OpenWeather.org API. It provides a 5 upcoming day weather forecast with temperature and weather conditions. It also contains a contact form with validation methods and dynamic outputs to user. It is hosted on the Firebase CLI. It also has a motion react. It is build with pure React and CSS. ",
+    img: "https://i.ibb.co/fGQBQBg/Screenshot-2022-07-17-at-06-04-32.png",
     links: {
-        site: '',
-        repo: ''
-    }
-},
+      site: "https://weather-app-deploy-hosting.web.app/",
+      repo: "https://github.com/Majchrzak202/ForecastApp-/tree/master",
+    },
+  },
 
-{
+  {
     id: 2,
     title: "Second best App",
     descritpion: "This is the coolest app ever made",
     img: "https://ui-lib.com/blog/wp-content/uploads/2022/04/john-minimal-one-page-1.png",
     links: {
-        site: '',
-        repo: ''
-    }
-},
+      site: "https://weather-app-deploy-hosting.web.app/",
+      repo: "https://github.com/Majchrzak202/ForecastApp-/tree/master",
+    },
+  },
 
-{
+  {
     id: 3,
     title: "Outstanding App",
     descritpion: "This is the coolest app ever made",
     img: "https://ui-lib.com/blog/wp-content/uploads/2022/04/john-minimal-one-page-1.png",
     links: {
-        site: '',
-        repo: ''
-    }
-}
+      site: "",
+      repo: "",
+    },
+  },
 ];
