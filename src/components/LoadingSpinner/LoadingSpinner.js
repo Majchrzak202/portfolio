ffe97ef420bd35dc56983f64f0670a2173aa88ea
MarcinMaster <m.luba505@gmail.com>
2022-08-08 07:52:24 +0200

    Added a cool MUI LoadingSpinner

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { CircularProgress, Box } from "@material-ui/core";
+import './LoadingSpinner.css'
+
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner">
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
