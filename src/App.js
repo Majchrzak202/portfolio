e1d702fe1d991e3e8ff2bf055e69e21ab15ef814
MarcinMaster <m.luba505@gmail.com>
2022-09-14 18:07:16 +0200

    Lazy loading of Routes Component and general cleaning before deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { lazy, Suspense } from "react";
-
-import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
-import HomeSecond from "./components/Pages/HomeSecond";
-
+import Home from "./components/Pages/homepage/Home";
+import Footer from "./components/footer/Footer";
 import "./App.css";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import CustomRoutes from "./components/Pages/CustomRoutes";
+
 import { useTheme } from "./components/context/ThemeContextProvider";
-import ProjectPage from "./components/projects/ProjectPage";
+/* import Resume from "./components/Pages/resume/Resume"; */
 
-const Home = lazy(() => import("./components/Pages/Home"));
-const About = lazy(() => import("./components/Pages/About"))
-const Projects = lazy(() => import("./components/Pages/Projects"))
-const Resume = lazy(() => import("./components/Pages/Resume"))
+const About = lazy(() => import("./components/Pages/about/About"));
+const Projects = lazy(() => import("./components/Pages/projects/Projects"));
+ const Resume = lazy(() => import("./components/Pages/resume/Resume"));
+const ProjectPage = lazy(() => import("./components/projects/ProjectPage"));
+
 
 const App = () => {
   const { theme } = useTheme();
@@ -22,15 +23,14 @@ const App = () => {
   return (
     <div className="App" id={theme}>
       <Navbar />
-      <Suspense fallback={<LoadingSpinner/>}>
-        <Routes>
-          
-          <Route path="/" element={<HomeSecond />}></Route>
+      <Suspense fallback={<LoadingSpinner />}>
+        <CustomRoutes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
           <Route path="/resume" element={<Resume />}></Route>
           <Route path="/project/:id" element={<ProjectPage />}></Route>
-        </Routes>
+        </CustomRoutes>
       </Suspense>
       <Footer />
     </div>
