2f5c560464e1e12e52d990dd23dc7cc505e53fae
MarcinMaster <m.luba505@gmail.com>
2022-09-14 18:17:43 +0200

    Movved styles to Styles.js

---
+++ src/components/Pages/homepage/Home.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { Grid, Typography, Box } from "@material-ui/core";
+import MainPhoto from "./../../../assets/MainPhoto.png";
+import { motion } from "framer-motion";
+import useStyles from "./Styles";
+import { useTheme } from "../../context/ThemeContextProvider";
+import { TypeAnimation } from "react-type-animation";
+import SocialIcons from "../../socialIcons/SocialIcons";
+
+const Home = () => {
+  const classes = useStyles();
+  const { theme } = useTheme();
+  return (
+    <div className={classes.main}>
+      <Grid id={theme} className={classes.container} container>
+        <Grid
+          id={theme}
+          className={classes.item}
+          xs={12}
+          sm={12}
+          md={7}
+          lg={7}
+          xl={8}
+          xxl={8}
+          item
+        >
+          <Box id={theme} className={classes.card}>
+            <motion.div transition={{ duration: 0.5 }} animate={{ y: -30 }}>
+              <Typography
+                className={classes.title}
+                style={{
+                  fontSize: "40px",
+                  marginLeft: "20px",
+                  fontWeight: 800,
+                  fontStyle: "Open Sans",
+                  textAlign: "left",
+                }}
+                variant="h1"
+              >
+                Hi there
+                <motion.div
+                  style={{
+                    paddingLeft: "10px",
+                  }}
+                  animate={{ rotate: -20 }}
+                  transition={{
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    repeatDelay: 0.7,
+                    from: 1,
+                    duration: 1,
+                    ease: "easeInOut",
+                    type: "tween",
+                  }}
+                >
+                  👋
+                </motion.div>
+              </Typography>
+              <Typography
+                className={classes.title}
+                style={{
+                  fontSize: "40px",
+                  marginLeft: "20px",
+                  fontWeight: 800,
+                  fontStyle: "Open Sans",
+                  textAlign: "left",
+                }}
+                variant="h1"
+              >
+                I'm Marcin Łuba
+              </Typography>
+
+              <TypeAnimation
+                sequence={[
+                  "Junior React Developer",
+                  2000, // Waits 1s
+                  "ServiceNow Consultant",
+                  2000, // Waits 2s
+                  "Self learner", //
+                  2000,
+                ]}
+                wrapper="div"
+                cursor={true}
+                repeat={Infinity}
+                className={classes.typeAnimation}
+              />
+              <h1
+                style={{
+                  fontSize: "20px",
+                  marginLeft: "20px",
+                  fontWeight: 800,
+                  fontStyle: "Open Sans",
+                  textAlign: "left",
+                  marginTop: "30px",
+                }}
+              >
+                Feel free to Conect with me:
+              </h1>
+            </motion.div>
+            <Box className={classes.socialBox}>
+              <SocialIcons />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+          xs={12}
+          sm={12}
+          md={5}
+          lg={5}
+          xl={4}
+          xxl={4}
+          item
+        >
+          <motion.div
+            transition={{ duration: 0.5 }}
+            animate={{ y: -40, x: -30 }}
+          >
+            <img className={classes.mainImage} src={MainPhoto} alt="Main" />
+          </motion.div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Pages/homepage/Styles.js
@@ -0,0 +1,103 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    minHeight: "80vh",
+    minWidth: "100vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    marginTop: "50px",
+  },
+  container: {
+    height: "80%",
+    width: "100%",
+    marginTop: "100px",
+    "& #Light": {
+      backgroundColor: "rgb(242, 244, 246)",
+    },
+    "& #Dark": {
+      backgroundColor: "black",
+    },
+  },
+  motionDiv: {
+    marginBottom: "30px",
+    marginLeft: "20px",
+    width: "60vw",
+    [theme.breakpoints.down("xs")]: {
+      width: "90vw",
+    },
+  },
+  item: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    height: "100%",
+    flexDirection: "column",
+    width: "100vw",
+    border: "none",
+    boxShadow: "none",
+    paddingLeft: "30px",
+    [theme.breakpoints.down("xs")]: {
+      height: "300px",
+      paddingLeft: "10px",
+    },
+    [theme.breakpoints.only("sm")]: {
+      alignItems: "center",
+    },
+  },
+
+  title: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    paddingBottom: "10px",
+    [theme.breakpoints.down("xs")]: {
+      textAlgin: "center",
+    },
+  },
+
+  typeAnimation: {
+    fontSize: "40px",
+    fontStyle: "Open Sans",
+    fontWeight: 700,
+    marginLeft: "15px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "26px",
+    },
+  },
+
+  mainImage: {
+    width: "400px",
+    height: "400px",
+    borderRadius: "99%",
+    backgroundColor: "rgba(40,48,62,255)",
+    marginTop: "30px",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "60px",
+      width: "300px",
+      height: "300px",
+    },
+  },
+
+  socialBox: {
+    width: "auto",
+    marginLeft: "30px",
+    padding: "5px",
+    marginBottom: "30px",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "0px",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: "40px",
+    },
+  },
+}));
+
+export default useStyles;
