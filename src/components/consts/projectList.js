f492199081b972e1b8f04f814526edab4a967528
MarcinMaster <m.luba505@gmail.com>
2022-09-28 07:46:00 +0200

    Updated images for Porjects

--- src/components/consts/projectList.js
+++ src/components/consts/projectList.js
@@ -5,7 +5,7 @@ export const PROJECTS = [
     descritpion: "Customer Project",
     longDescritption:
       "Customer Project of Financial Portfolio made with pure React. It is mainly designed with Material UI. It provides a functional Contact Form integrated with EmailJS. It uses React Router.  ",
-    img: "https://i.ibb.co/kxLBbkL/Screenshot-2022-09-14-at-08-26-04.png",
+    img: "https://i.ibb.co/Pw30cHp/Screenshot-2022-09-28-at-07-41-25.png",
     links: {
       site: "https://filipfinanse.netlify.app/",
       repo: "https://github.com/Majchrzak202/creditofficersite",
@@ -18,7 +18,7 @@ export const PROJECTS = [
     descritpion: "Beer Search React App",
     longDescritption:
       "Beer App build with React, Material UI and CSS. It uses punkapi to fetch Beers. I have used here React Router, Lazy Loading, Loading Spinnder made with MUI.Ive also leveraged custom Hook for contact form validation. In this project i also used useEffect, useState and other core React features.  ",
-    img: "https://i.ibb.co/2jQj3bt/Screenshot-2022-08-12-at-18-20-35.png",
+    img: "https://i.ibb.co/yhBQTht/Screenshot-2022-09-28-at-07-37-57.png",
     links: {
       site: "https://beerworldapp.web.app/",
       repo: "https://github.com/Majchrzak202/beer-app",
@@ -31,7 +31,7 @@ export const PROJECTS = [
     descritpion: "Weather forecast React App ",
     longDescritption:
       "Simple React Weather App that uses React Routing, Custom Hooks and connects to OpenWeather.org API. It provides a 5 upcoming day weather forecast with temperature and weather conditions. It also contains a contact form with validation methods and dynamic outputs to user. It is hosted on the Firebase CLI. It also has a motion react. It is build with pure React and CSS. ",
-    img: "https://i.ibb.co/fGQBQBg/Screenshot-2022-07-17-at-06-04-32.png",
+    img: "https://i.ibb.co/fH3QBGq/Screenshot-2022-09-28-at-07-38-21.png",
     links: {
       site: "https://weather-app-deploy-hosting.web.app/",
       repo: "https://github.com/Majchrzak202/ForecastApp-/tree/master",
