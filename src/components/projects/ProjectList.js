842515c260d26ddd326941384c5a0c6f7963ebd5
MarcinMaster <m.luba505@gmail.com>
2022-09-28 07:11:21 +0200

    Some minor style adjustments

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import { PROJECTS } from "../consts/projectList";
-import ProjectCard from "./ProjectCard/ProjectCard";
+import ProjectCard from "./project-card/ProjectCard";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: "5px",
+    [theme.breakpoints.up(1800)]: {
+      width: "85%",
+      height: "50vh",
+    },
+  },
+}));
 
 const ProjectList = () => {
+  const classes = useStyles();
   return (
     <Grid
       container
       justifyContent="center"
       spacing={2}
-      style={{ padding: "5px" }}
+      className={classes.container}
     >
       {PROJECTS.map((project) => (
         <Grid key={project.id} item xs={12} sm={4} md={4} lg={4} xl={4}>
@@ -31,4 +43,4 @@ const ProjectList = () => {
   );
 };
 
-export default ProjectList;
+export default React.memo(ProjectList);
