68531660f75f06eee7fc447aa96a12333bc75513
MarcinMaster <m.luba505@gmail.com>
2022-09-28 07:12:00 +0200

    Switched from Box to Grid model to adjust the sizing of Icons on mobile

--- src/components/socialIcons/Styles.js
+++ src/components/socialIcons/Styles.js
@@ -3,9 +3,8 @@ import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles({
   socialIcons: {
     "& #Light": {
-      /* fill: "white", */
       backgroundColor: "rgba(251, 251, 251, 0);",
-      color: 'white'
+      color: "white",
     },
     "& #Dark": {
       fill: "white",
